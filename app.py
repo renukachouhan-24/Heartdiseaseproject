1bf48d99fafbaf1b102736a20a5db18fd812bf5d
Renuka <renukachouhan24@navgurukul.org>
2026-01-08 16:23:10 +0530

    correct error

--- app.py
+++ app.py
@@ -31,4 +31,6 @@ def predict():
             return render_template('index.html', prediction_text=f'Error: Please check your inputs.')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
