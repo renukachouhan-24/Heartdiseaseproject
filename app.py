f0da45deda52c9f91df50582bbf63fbf12ac148f
Renuka <renukachouhan24@navgurukul.org>
2026-01-09 11:51:13 +0530

    solved error

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Model load karein jo aapne abhi banaya hai
 model = joblib.load('best_svm_model.pkl')
 
 @app.route('/')
@@ -14,13 +13,10 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        # Form se saari values lena [01:13:30]
-        # Video ke according 13 features hain
         try:
             features = [float(x) for x in request.form.values()]
             final_features = [np.array(features)]
             
-            # Prediction karna
             prediction = model.predict(final_features)
             
             output = "Risk of Heart Disease" if prediction[0] == 1 else "No Heart Disease"
